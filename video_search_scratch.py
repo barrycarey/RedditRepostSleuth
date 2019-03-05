b6692b75bc21270f6778af7e08543328146201fe
barry <mcarey66@gmail.com>
2019-03-05 08:29:29 -0500

    Working on video post matching

--- video_search_scratch.py
+++ video_search_scratch.py
@@ -1,29 +1,59 @@
 import sys
+import os
 
 import ffmpeg
 
-probe = ffmpeg.probe('DASH_9_6_M.mp4')
+from redditrepostsleuth.db import db_engine
+from redditrepostsleuth.db.uow.sqlalchemyunitofworkmanager import SqlAlchemyUnitOfWorkManager
+from redditrepostsleuth.util.helpers import get_reddit_instance
+
+reddit = get_reddit_instance()
+
+sub = reddit.submission(id='akixg2')
 
 
-interval = float(probe['format']['duration']) / 20
-count = 1
-seek = interval
-while count <= 20:
 
 
-    try:
-        (
-            ffmpeg
-                .input('https://v.redd.it/codqh832t2d21/DASH_9_6_M?source=fallback', ss=seek)
-                .filter('scale', 720, -1)
-                .output('{}.png'.format(str(count)), vframes=1)
-                .overwrite_output()
-                .run(capture_stdout=True, capture_stderr=True)
-        )
-    except ffmpeg.Error as e:
-        print(e.stderr.decode(), file=sys.stderr)
-        sys.exit(1)
+
+uowm = SqlAlchemyUnitOfWorkManager(db_engine)
+
+with uowm.start() as uow:
+    posts = uow.posts.find_all_by_type('hosted:video', limit=50)
+
+for post in posts:
+    sub = reddit.submission(id=post.post_id)
+    probe = ffmpeg.probe(sub.media['reddit_video']['fallback_url'])
+    out_dir = os.path.join(os.getcwd(), 'video', post.post_id)
+    os.mkdir(out_dir)
+
+    interval = float(probe['format']['duration']) / 20
+    count = 1
+    seek = interval
+
+    (
+        ffmpeg
+            .input(sub.media['reddit_video']['fallback_url'])
+            .filter('scale', 720, -1)
+            .output(os.path.join(out_dir, '{}.png'.format(str(count))), vframes=1)
+            .overwrite_output()
+            .run(capture_stdout=True, capture_stderr=True)
+    )
+
+    while count <= 20:
+
+        try:
+            (
+                ffmpeg
+                    .input(sub.media['reddit_video']['fallback_url'], ss=seek)
+                    .filter('scale', 720, -1)
+                    .output(os.path.join(out_dir, '{}.png'.format(str(count))), vframes=1)
+                    .overwrite_output()
+                    .run(capture_stdout=True, capture_stderr=True)
+            )
+        except ffmpeg.Error as e:
+            print(e.stderr.decode(), file=sys.stderr)
+            sys.exit(1)
 
 
-    seek += interval
-    count += 1
+        seek += interval
+        count += 1
