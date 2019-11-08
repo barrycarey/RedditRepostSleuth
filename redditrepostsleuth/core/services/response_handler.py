b55f2c5cc1080f76de48960cd7eea7d02be80481
Carey, Matthew <Matthew.Carey6@t-mobile.com>
2019-11-08 10:03:24 -0500

    Add response manager

--- redditrepostsleuth/core/services/response_handler.py
+++ redditrepostsleuth/core/services/response_handler.py
@@ -1,22 +1,34 @@
-from praw import Reddit
-
-from redditrepostsleuth.core.db.databasemodels import Post
 from redditrepostsleuth.core.db.uow.unitofworkmanager import UnitOfWorkManager
+from redditrepostsleuth.core.logging import log
+from redditrepostsleuth.core.services.eventlogging import EventLogging
+from redditrepostsleuth.core.services.reddit_manager import RedditManager
 
 
 class ResponseHandler:
 
-    def __init__(self, reddit: Reddit, uowm: UnitOfWorkManager):
+    def __init__(self, reddit: RedditManager, uowm: UnitOfWorkManager, event_logger: EventLogging, log_response: bool = True):
         self.uowm = uowm
         self.reddit = reddit
+        self.log_response = log_response
 
-    def leave_comment(self, post_id: str) -> bool:
+    def leave_comment(self, submission_id: str, comment_body, send_pm_on_fail: bool = False) -> bool:
+        submission = self.reddit.submission(submission_id)
+        if not submission:
+            return False
+
+        try:
+            submission.reply(comment_body)
+            self._log_response(comment_body, 'comment')
+        except Exception as e:
+            log.exception('Failed to leave comment on post https://redd.it/%s', submission_id)
+            if send_pm_on_fail:
+                pass
+
+
+    def send_private_message(self, user: str, message_body) -> bool:
         pass
 
-    def send_private_message(self, user: str):
-        pass
-
-    def _log_response(self, response):
+    def _log_response(self, response, response_type: str):
         self._log_response_to_db(response)
         self._log_response_to_influxdb(response)
 
@@ -32,4 +44,7 @@ class ResponseHandler:
         Take a given response and log it to the database
         :param response:
         """
-        pass
+        with self.uowm.start() as uow:
+            uow.bot_comment.add(
+
+            )
