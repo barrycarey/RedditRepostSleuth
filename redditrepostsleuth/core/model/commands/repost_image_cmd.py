0de6c65aa11e940736525d88de960fd29c165cf1
barry <mcarey66@gmail.com>
2019-11-15 20:40:08 -0500

    command parser working on images

--- redditrepostsleuth/core/model/commands/repost_image_cmd.py
+++ redditrepostsleuth/core/model/commands/repost_image_cmd.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 
+from redditrepostsleuth.core.model.commands.repost_base_cmd import RepostBaseCmd
+
 
 @dataclass
-class RepostImageCmd:
+class RepostImageCmd(RepostBaseCmd):
     meme_filter: bool
     strictness: int
-    same_sub: bool
-    all_matches: bool
