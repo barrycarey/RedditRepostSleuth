f431dff720fde581985077c36262706ba37073ef
Barry Carey <mcarey66@gmail.com>
2019-11-17 07:50:20 -0500

    Working status update.  Fixes #42

--- redditrepostsleuth/adminsvc/adminsvc.py
+++ redditrepostsleuth/adminsvc/adminsvc.py
@@ -1,20 +1,23 @@
 import sys
 import time
 
+from redditrepostsleuth.adminsvc.stats_updater import StatsUpdater
 from redditrepostsleuth.core.config import Config
 from redditrepostsleuth.core.db.db_utils import get_db_engine
 from redditrepostsleuth.core.db.uow.sqlalchemyunitofworkmanager import SqlAlchemyUnitOfWorkManager
 from redditrepostsleuth.core.services.reddit_manager import RedditManager
 from redditrepostsleuth.core.util.reddithelpers import get_reddit_instance
-from redditrepostsleuth.statssvc.bot_comment_monitor import BotCommentMonitor
+from redditrepostsleuth.adminsvc.bot_comment_monitor import BotCommentMonitor
 
 sys.path.append('./')
 
 if __name__ == '__main__':
     while True:
-        config = Config('/home/barry/PycharmProjects/RedditRepostSleuth/sleuth_config.json')
+        config = Config()
         uowm = SqlAlchemyUnitOfWorkManager(get_db_engine(config))
         reddit_manager = RedditManager(get_reddit_instance(config))
         comment_monitor = BotCommentMonitor(reddit_manager, uowm, config)
         comment_monitor.check_comments()
+        stats_updater = StatsUpdater()
+        stats_updater.run_update()
         time.sleep(1800)
