81929424e4ff25cec1ada9c31320ae4bf64be3d9
Carey, Matthew <Matthew.Carey6@t-mobile.com>
2019-11-18 18:36:59 -0500

    Fixing tests

--- tests/summonssvc/commandparsing/test_commandParser.py
+++ tests/summonssvc/commandparsing/test_commandParser.py
@@ -7,17 +7,17 @@ from redditrepostsleuth.summonssvc.commandparsing.command_parser import CommandP
 
 class TestCommandParser(TestCase):
     def test_parse_repost_cmd(self):
-        parser = CommandParser()
-        r = parser.parse_repost_image_cmd('test -meme -strict tight')
+        parser = CommandParser(config=Config(summons_match_strictness_tight=9))
+        r = parser.parse_repost_image_cmd('test -meme -matching tight')
         self.assertTrue(r.meme_filter)
-        self.assertEqual(r.strictness, 'tight')
+        self.assertEqual(r.strictness, 9)
         self.assertFalse(r.same_sub)
 
     def test_parse_root_command__valid_command(self):
         parser = CommandParser(config=Config(redis_host='dummy'))
-        r = parser.parse_root_command('repost -meme -strict tight')
+        r = parser.parse_root_command('repost -meme -matching tight')
         self.assertEqual('repost', r)
 
     def test_parse_root_command__invalid_command(self):
         parser = CommandParser(config=Config(redis_host='dummy'))
-        self.assertRaises(InvalidCommandException, parser.parse_root_command, 'junk -meme -strict tight')
+        self.assertRaises(InvalidCommandException, parser.parse_root_command, 'junk -meme -matching tight')
