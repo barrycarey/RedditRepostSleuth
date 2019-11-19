846254c9ac4f902b401a64bb89ffb9a35d6a7a24
barry <mcarey66@gmail.com>
2019-11-19 08:16:12 -0500

    Filters built

--- tests/core/util/test_repost_filters.py
+++ tests/core/util/test_repost_filters.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from unittest import TestCase
+from unittest.mock import patch
 
 from redditrepostsleuth.core.db.databasemodels import Post
 from redditrepostsleuth.core.model.imagematch import ImageMatch
 from redditrepostsleuth.core.util.repost_filters import cross_post_filter, same_sub_filter, annoy_distance_filter, \
-    hamming_distance_filter
+    hamming_distance_filter, filter_newer_matches, filter_days_old_matches, filter_same_author
 
 
 class TestCross_post_filter(TestCase):
@@ -35,7 +37,7 @@ class TestCross_post_filter(TestCase):
         self.assertEqual(len(r), 1)
         self.assertEqual(2, r[0].match_id)
 
-    def annoy_distance_filter__remove_higher_distance(self):
+    def test_hamming_distance_filter__remove_higher_distance(self):
         matches = []
         match1 = ImageMatch()
         match2 = ImageMatch()
@@ -52,3 +54,77 @@ class TestCross_post_filter(TestCase):
         r = list(filter(hamming_distance_filter(11), matches))
         self.assertEqual(len(r), 1)
         self.assertEqual(1, r[0].match_id)
+
+    def test_annoy_distance_filter__remove_higher_distance(self):
+        matches = []
+        match1 = ImageMatch()
+        match2 = ImageMatch()
+        match3 = ImageMatch()
+        match1.match_id = 1
+        match2.match_id = 2
+        match3.match_id = 3
+        match1.hamming_distance = 0.100
+        match2.annoy_distance = 0.200
+        match3.annoy_distance = 0.250
+        matches.append(match1)
+        matches.append(match2)
+        matches.append(match3)
+        r = list(filter(annoy_distance_filter(0.150), matches))
+        self.assertEqual(len(r), 1)
+        self.assertEqual(1, r[0].match_id)
+
+    def test_filter_newer_matches__remove_newer(self):
+        matches = []
+        match1 = ImageMatch()
+        match2 = ImageMatch()
+        match3 = ImageMatch()
+        match1.match_id = 1
+        match2.match_id = 2
+        match3.match_id = 3
+        match1.post = Post(created_at=datetime.utcfromtimestamp(1574168050))
+        match2.post = Post(created_at=datetime.utcfromtimestamp(1574081650))
+        match3.post = Post(created_at=datetime.utcfromtimestamp(1573908850))
+        matches.append(match1)
+        matches.append(match2)
+        matches.append(match3)
+        r = list(filter(filter_newer_matches(datetime.utcfromtimestamp(1573995250)), matches))
+        self.assertEqual(len(r), 1)
+        self.assertEqual(3, r[0].match_id)
+
+    def test_filter_days_old_matches__remove_older(self):
+        matches = []
+        match1 = ImageMatch()
+        match2 = ImageMatch()
+        match3 = ImageMatch()
+        match1.match_id = 1
+        match2.match_id = 2
+        match3.match_id = 3
+        match1.post = Post(created_at=datetime.utcfromtimestamp(1571349660))
+        match2.post = Post(created_at=datetime.utcfromtimestamp(1571090460))
+        match3.post = Post(created_at=datetime.utcfromtimestamp(1570917660))
+        matches.append(match1)
+        matches.append(match2)
+        matches.append(match3)
+        with patch('redditrepostsleuth.core.util.repost_filters.datetime') as mock_date:
+            mock_date.utcnow.return_value = datetime.utcfromtimestamp(1571360460)
+            r = list(filter(filter_days_old_matches(2), matches))
+        self.assertEqual(len(r), 1)
+        self.assertEqual(1, r[0].match_id)
+
+    def test_filter_newer_matches__remove_newer(self):
+        matches = []
+        match1 = ImageMatch()
+        match2 = ImageMatch()
+        match3 = ImageMatch()
+        match1.match_id = 1
+        match2.match_id = 2
+        match3.match_id = 3
+        match1.post = Post(author='barry')
+        match2.post = Post(author='barry')
+        match3.post = Post(author='steve')
+        matches.append(match1)
+        matches.append(match2)
+        matches.append(match3)
+        r = list(filter(filter_same_author('barry'), matches))
+        self.assertEqual(len(r), 1)
+        self.assertEqual(3, r[0].match_id)
