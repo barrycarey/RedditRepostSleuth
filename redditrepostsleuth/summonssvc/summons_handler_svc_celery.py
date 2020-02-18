5571fc7a1df716d839bee6e5b397026f5622ba22
barry <mcarey66@gmail.com>
2020-02-18 06:38:49 -0500

    Fix for top post

--- redditrepostsleuth/summonssvc/summons_handler_svc_celery.py
+++ redditrepostsleuth/summonssvc/summons_handler_svc_celery.py
@@ -1,7 +1,7 @@
 # TODO - Mega hackery, figure this out.
 import sys
 import time
-
+from redis.exceptions import ConnectionError
 
 
 sys.path.append('./')
@@ -28,18 +28,24 @@ if __name__ == '__main__':
     response_builder = ResponseBuilder(uowm)
     reddit_manager = RedditManager(get_reddit_instance(config))
     summons = SummonsHandler(uowm, dup, reddit_manager, response_builder, ResponseHandler(reddit_manager, uowm, event_logger, source='summons'), event_logger=event_logger, summons_disabled=False)
-
     while True:
-        with uowm.start() as uow:
-            summons = uow.summons.get_unreplied(limit=10)
-            results = []
-            for s in summons:
-                log.info('Starting summons %s', s.id)
-                results.append(handle_summons.apply_async((s,), queue='summons'))
-            for r in results:
-                b = r.get()
-                #time.sleep(10)
-        time.sleep(2)
+        try:
+            with uowm.start() as uow:
+                summons = uow.summons.get_unreplied(limit=10)
+                results = []
+                for s in summons:
+                    log.info('Starting summons %s', s.id)
+                    results.append(handle_summons.apply_async((s,), queue='summons'))
+                for r in results:
+                    try:
+                        b = r.get()
+                    except ConnectionError:
+                        time.sleep(60)
+                        b = r.get()
+                    #time.sleep(10)
+            time.sleep(2)
+        except ConnectionError as e:
+            log.exception('Error connecting to Redis')
 
     """
     while True:
