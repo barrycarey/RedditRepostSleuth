d638f2047a8be7ea5178edde2ad2ba21d56bf4a8
barry <mcarey66@gmail.com>
2020-04-12 15:25:26 -0400

    Use list of subs to force PM response.  Fixes #145

--- redditrepostsleuth/core/config.py
+++ redditrepostsleuth/core/config.py
@@ -146,6 +146,7 @@ class Config:
             'summons_all_matches',
             'summons_same_sub',
             'summons_max_age',
+            'summons_send_pm_subs',
             'bot_comment_karma_flag_threshold',
             'bot_comment_karma_remove_threshold',
             'sub_monitor_exposed_config_options'
