ff7dd7f4f63d1f51a5f94299025472362c4c2aa7
barry <mcarey66@gmail.com>
2020-09-21 23:24:47 -0400

    More API tweaks

--- redditrepostsleuth/repostsleuthsiteapi/endpoints/post_watch.py
+++ redditrepostsleuth/repostsleuthsiteapi/endpoints/post_watch.py
@@ -2,6 +2,7 @@ import json
 
 from falcon import Request, Response, HTTPNotFound, HTTPUnauthorized
 
+from redditrepostsleuth.core.db.databasemodels import RepostWatch
 from redditrepostsleuth.core.db.uow.unitofworkmanager import UnitOfWorkManager
 from redditrepostsleuth.core.util.reddithelpers import get_user_data, is_sleuth_admin
 
@@ -44,10 +45,33 @@ class PostWatch:
             uow.commit()
 
     def on_delete(self, req: Request, resp: Response):
-        pass
+        token = req.get_param('token', required=True)
+        watch_id = req.get_param_as_int('watch_id', required=True)
+        user_data = get_user_data(token)
+        with self.uowm.start() as uow:
+            watch = uow.repostwatch.get_by_id(watch_id)
+            if not watch:
+                raise HTTPNotFound(title='No watch found', description=f'Failed to find watch with ID {data["id"]}')
+            if watch.user.lower() != user_data['name'].lower():
+                if not is_sleuth_admin(token, user_data):
+                    raise HTTPUnauthorized(title='You are not authorized to delete this watch',
+                                           description='You are not authorized to delete this post watch')
+            uow.repostwatch.remove(watch)
+            uow.commit()
+
 
     def on_post(self, req: Request, resp: Response):
-        pass
+        token = req.get_param('token', required=True)
+        user_data = get_user_data(token)
+        data = json.load(req.bounded_stream)
+        with self.uowm.start() as uow:
+            watch = RepostWatch(
+                post_id=data['post_id'],
+                user=user_data['name'],
+                source='site'
+            )
+            uow.repostwatch.add(watch)
+            uow.commit()
 
     def on_get_single(self):
         pass
