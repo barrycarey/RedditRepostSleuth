c8979798284ec1533936874b79e1fe6bd9cac5e6
Matthew Carey <mcarey66@gmail.com>
2024-12-09 20:54:14 -0500

    Feature/store subreddit data (#382)
    * ingest save time tracking by post type
    
    * Store subreddit data
    
    * Store subreddit data

--- redditrepostsleuth/core/celery/celeryconfig.py
+++ redditrepostsleuth/core/celery/celeryconfig.py
@@ -16,6 +16,7 @@ worker_log_color = None
 task_routes = {
     'redditrepostsleuth.core.celery.tasks.ingest_tasks.save_new_post': {'queue': 'post_ingest'},
     'redditrepostsleuth.core.celery.tasks.ingest_tasks.save_new_posts': {'queue': 'post_ingest'},
+    'redditrepostsleuth.core.celery.tasks.ingest_tasks.save_subreddit': {'queue': 'save_subreddit'},
     'redditrepostsleuth.core.celery.tasks.ingest_tasks.ingest_repost_check': {'queue': 'repost'},
     'redditrepostsleuth.core.celery.tasks.repost_tasks.check_image_repost_save': {'queue': 'repost_image'},
     'redditrepostsleuth.core.celery.tasks.repost_tasks.link_repost_check': {'queue': 'repost_link'},
@@ -31,6 +32,8 @@ task_routes = {
     'redditrepostsleuth.core.celery.admin_tasks.update_subreddit_config_from_database': {'queue': 'update_wiki_from_database'},
     #'redditrepostsleuth.core.celery.admin_tasks.delete_search_batch': {'queue': 'batch_delete_searches'},
     'redditrepostsleuth.core.celery.tasks.reddit_action_tasks.*': {'queue': 'reddit_actions'},
+    'redditrepostsleuth.core.celery.tasks.maintenance_tasks.update_subreddit_data': {'queue': 'update_subreddit_data'},
+    'redditrepostsleuth.core.celery.tasks.maintenance_tasks.save_subreddit': {'queue': 'update_subreddit_data'}
 
 
 }
